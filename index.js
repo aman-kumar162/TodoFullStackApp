2964aa67e74a9acc809b3b7d6728f81ba1954a88
Aman Kumar <amarraj8571841168@gmail.com>
2024-07-26 07:25:41 +0530

    third commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express=require('express')
+const { createTodo, updateTodo } = require('./types')
 
 const app=express()
 const PORT=3000
@@ -6,13 +7,34 @@ const PORT=3000
 app.use(express.json())
 
 app.post("/todo",(req,res)=>{
-    
+     const createPayload=req.body;
+     const parsedPayload=createTodo.safeParse(createPayload);
+     if(!parsedPayload.success){
+        res.status(411).json({
+            msg:"You sent wrong inputs",
+        })
+        return;
+     }
+
+     TodoistProvider.create({
+        parsedPayload
+     });
+     res.json({
+        message:"success"
+     })
+      
 })
 app.get("/todos",(req,res)=>{
-
+         
 })
 app.put("/completed",(req,res)=>{
-
+      const updatePayload=req.body;
+      const parsedPayload=updateTodo.safeParse(updatePayload)
+      if(!parsedPayload){
+        res.status(411).json({
+            msg:"You Sent the wrong inputs"
+        })
+      }
 })
 
 
